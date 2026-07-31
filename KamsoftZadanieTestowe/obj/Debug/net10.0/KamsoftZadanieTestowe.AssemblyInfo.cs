9ed17790aa013d36fbc4f521a8960c7bb88e9fc3
Kuba <kuba.pocztowski99@gmail.com>
2026-07-31 20:29:23 +0200

    fixes

--- KamsoftZadanieTestowe/obj/Debug/net10.0/KamsoftZadanieTestowe.AssemblyInfo.cs
+++ KamsoftZadanieTestowe/obj/Debug/net10.0/KamsoftZadanieTestowe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KamsoftZadanieTestowe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156d612b70bf96632d8b91e3e2e748814faa1e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("KamsoftZadanieTestowe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KamsoftZadanieTestowe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
